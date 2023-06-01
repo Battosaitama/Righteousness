9e5bae025c02451d18312aeabed11c8029fdf6fd
John Rivera <116494821+Battosaitama@users.noreply.github.com>
2023-06-01 20:48:10 +0800

    Update Highlight.js

--- Highlight.js
+++ Highlight.js
@@ -4,9 +4,12 @@ const main = document.getElementById('main');
 let parts = [];
 
 let images = [
-  "https://images.unsplash.com/photo-1549880338-65ddcdfd017b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80",
-  "https://images.unsplash.com/photo-1544198365-f5d60b6d8190?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80",
-  "https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2700&q=80"
+  "https://battosaitama.github.io/Righteousness/Group%20pics/23.jpg",
+  "https://battosaitama.github.io/Righteousness/Group%20pics/38.jpg",
+  "https://battosaitama.github.io/Righteousness/Group%20pics/35.jpg",
+  "https://battosaitama.github.io/Righteousness/Group%20pics/39.jpg",
+  "https://battosaitama.github.io/Righteousness/Group%20pics/group%20pic%208.jpg",
+  "https://battosaitama.github.io/Righteousness/Group%20pics/32.jpg"
 ];
 let current = 0;
 let playing = false;
